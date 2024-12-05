6d866b35d64af5830af5caf7d33dd8f28ae55a81
LÊ THIÊN TRÍ <thientrile2003@gmail.com>
2024-12-05 13:03:30 +0700

    upgrade to https

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,29 @@
 /** @format */
 
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config';
 
+import fs from 'fs';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@vite-pwa/nuxt', '@nuxt/icon',  '@pinia/nuxt'],
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@vite-pwa/nuxt',
+		'@nuxt/icon',
+		'@pinia/nuxt'
+	],
 	runtimeConfig: {
-		app:{
+		app: {
 			apiUrl: `${process.env.API_SCHEME}://${process.env.API_HOST}:${process.env.API_PORT}/${process.env.API_VERSION}/${process.env.API_NAME}`,
-			apiKey: process.env.API_KEY,
+			apiKey: process.env.API_KEY
 		}
-		
+	}
+	,
+	devServer: {
+		https: {
+			key: './key.pem',
+			cert: './cert.pem'
+		}
 	}
 });
